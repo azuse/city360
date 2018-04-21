3ac44fe02a320e91b7ec94662e22cf9723cfbefc
azuse <misakaxindex@gmail.com>
2018-04-21 16:57:32 +0800

    php mysql update

--- php/upload.php
+++ php/upload.php
@@ -1,4 +1,30 @@
 <?php 
+
+	class Item{
+	    public $author,$street,$tel,$budget,$demand,$address,$time,$lng,$lat;
+	    function __construct($author,$street,$tel,$budget,$demand,$address,$time,$lng,$lat){
+	        $this->author = $author;
+	        $this->street = $street;
+	        $this->tel = $tel;
+	        $this->budget = $budget;
+	        $this->demand = $demand;
+	        $this->address = $address;
+	        $this->time = $time;
+	        $this->lng = $lng;
+	        $this->lat = $lat;
+	    }
+	    function show_data(){
+	        echo "<author>$this->author</author>";
+	        echo "<street>$this->street</street>";
+	        echo "<tel>$this->tel</tel>";
+	        echo "<budget>$this->budget</budget>";
+	        echo "<demand>$this->damand</demand>";
+	        echo "<address>$this->address</address>";
+	        echo "<time>$this->time</time>";        
+	    }
+	}
+
+
     class DB{
         public $conn;
         function __construct()
@@ -8,9 +34,9 @@
             $password = "7172eaf610";
             $dbname = "city360";
             
-            $this->conn = mysqli_connect($dbhost,$account,$password,$dbname);        
+            $this->conn = mysql_connect($dbhost,$account,$password);        
             if (!$this->conn) {
-                die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+                // die('Connect Error (' . mysql_connect_errno() . ') '. mysql_connect_error());
             }else{
                 //echo 'Connect success... ' . mysqli_get_host_info($this->conn) . "\n";
             }
@@ -24,16 +50,25 @@
     }
 
     function saveData($item){
+    	$db = new DB();
+    	$sqlselect = "USE city360";
+        $result = mysql_query($sqlselect,$db->conn);
+    	 if(!$result){
+            echo "Data save fail:".mysql_error($db->conn);
+        }else{
+            echo "Data save successfully!!!";
+        }
+
         if($item->lat && $item->lng)
             $sql = "INSERT INTO `city360data_demo`(`author`,`street`,`tel`,`budget`,`demand`,`time`,`address`,`lng`,`lat`) VALUES ('".$item->author."','".$item->street."','".$item->tel."','".$item->budget."','".$item->demand."','".$item->time."','".$item->address."','".$item->lng."','".$item->lat."');";
         else
-            $sql = "INSERT INTO `city360data_demo`(`author`,`street`,`tel`,`budget`,`demand`,`time`,`address`) VALUES ('".$item->author."','".$item->street."','".$item->tel."','".$item->budget."','".$item->demand."','".$item->time."','".$item->address."');";
+            $sql = "INSERT INTO `city360data_demo`(`author`,`street`,`tel`,`budget`,`demand`,`time`,`address`,`lng`,`lat`) VALUES ('".$item->author."','".$item->street."','".$item->tel."','".$item->budget."','".$item->demand."','".$item->time."','".$item->address."','0','0');";
             
         // echo var_dump($this->db->conn);
-        $result = mysqli_query($this->db->conn,$sql);
+        $result = mysql_query($sql,$db->conn);
         
         if(!$result){
-            echo "Data save fail:".mysqli_error($this->db->conn);
+            echo "Data save fail:".mysql_error($db->conn);
         }else{
             echo "Data save successfully!!!";
         }
@@ -41,11 +76,9 @@
 
     $time = date("Y-m-d H:s:i");
     if($_SERVER['REQUEST_METHOD']=='POST'){
-        $item = new Item($_POST['author'],$_POST['street'],$_POST['tel'],$_POST['budget'],$_POST['demand'],$_POST['address'],$Time,$_POST['lng'],$_POST['lat']);
-        
+    	echo "1";
+        $item = new Item($_POST['author'],$_POST['street'],$_POST['tel'],$_POST['budget'],$_POST['demand'],$_POST['address'],$time,$_POST['lng'],$_POST['lat']);
+        echo "2";
         saveData($item);
-    }
-
-
-
+    };
  ?>
