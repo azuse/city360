ae3558c2699f67edeb87a1f3ee6b5783602cd3cf
azuse <misakaxindex@gmail.com>
2018-04-22 18:15:32 +0800

    upload链接

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $('.carousel.carousel-slider').carousel({full_width: true});
  $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+    if(GetQueryString("fun")=="upload")
+      $("#uploadscreen").modal('open');
   });
 
   
@@ -37,3 +39,22 @@ $('.carousel.carousel-slider').carousel({full_width: true});
     };
   
   map.addEventListener("click", showInfo);
+
+  function GetQueryString(param) { //param为要获取的参数名 注:获取不到是为null  
+    var currentUrl = window.location.href; //获取当前链接  
+    var arr = currentUrl.split("?");//分割域名和参数界限  
+    if (arr.length > 1) {  
+        arr = arr[1].split("&");//分割参数  
+        for (var i = 0; i < arr.length; i++) {  
+            var tem = arr[i].split("="); //分割参数名和参数内容  
+            if (tem[0] == param) {  
+                return tem[1];  
+            }  
+        }  
+        return null;  
+    }  
+    else {  
+        return null;  
+    }  
+}  
+
