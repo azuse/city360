011b5e1051bbd0573f02fd7f4c0abc7751f1a4e8
azuse <misakaxindex@gmail.com>
2018-04-24 17:01:10 +0800

    showall php

---
+++ php/download.php
@@ -0,0 +1,64 @@
+<?php
+
+class DB{
+    public $conn;
+    function __construct()
+    { //connect
+        $dbhost = "localhost";
+        $account = "root";
+        $password = "7172eaf610";
+        $dbname = "city360";
+        
+        $this->conn = mysql_connect($dbhost,$account,$password);        
+        if (!$this->conn) {
+            // die('Connect Error (' . mysql_connect_errno() . ') '. mysql_connect_error());
+        }else{
+            //echo 'Connect success... ' . mysqli_get_host_info($this->conn) . "\n";
+        }
+    
+    }
+    function __destruct()
+    {   //disconnect
+        //mysqli_colse($this->conn);//无此函数
+    }
+}
+
+function loadData(){
+    $db = new DB();
+    
+    global $dataBuf;
+    $sqlselect = "USE city360";
+    $result = mysql_query($sqlselect,$db->conn);
+
+    $sql = "SELECT * FROM `city360data_demo` ";
+    $result = mysqli_query($sql,$db->conn);
+    if($result == null){
+        echo "error:".mysqli_error($this->db->conn)."<br>";
+    }
+    //echo var_dump($result);
+    $i = 0;
+    while($row = mysqli_fetch_array($result))
+    {
+        //$temp = new Item($row['UID'],$row['Uname'],$row['Note'],$row['Longitude'],$row['Latitude'],$row['Altitude'],$row['Time']);
+        //先将结果放入一维数组中
+        $temp["author"] = $row['author'];
+        $temp["street"] = $row['street'];
+        $temp["tel"] = $row['tel'];
+        $temp["budget"] = $row['budget'];
+        $temp["demand"] = $row['demand'];
+        $temp["time"] = $row['time'];
+        $temp["address"] = $row['address'];
+        $temp["lng"] = $row['lng'];
+        $temp["lat"] = $row['lat'];
+        //放入二维数组dataBuf中
+        $dataBuf[$i++] = $temp;
+    }
+    
+    //输出json格式字符串
+    echo json_encode($dataBuf);        
+    mysqli_free_result($result);
+}
+
+loadData();
+
+?>
---
+++ showall/showall.js
@@ -0,0 +1,1 @@
+var map = new BMap.Map("bdmap");
