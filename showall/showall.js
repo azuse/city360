a9f00116ba7d03afb06bba6606b8d25d68ab3506
azuse <misakaxindex@gmail.com>
2018-05-03 22:05:18 +0800

    map point

--- showall/showall.js
+++ showall/showall.js
@@ -81,6 +81,10 @@ function newcard(name,author,street,tel,budget,demand,time,address,lng,lat,id){
         $("#detail_address")[0].innerHTML = '<i class="material-icons prefix margin14">place</i>'+address;
         $("#detail_window").modal('open');
         $("#detail_del")[0].textStatus = id;
+        var point = new BMap.point(lng,lat);
+        map.setCenter(point,12);
+        var marker = new BMap.Marker(point);
+        map.addOverlay(marker);
     };
     a.innerHTML = "详情"
     cardaction.appendChild(a);
