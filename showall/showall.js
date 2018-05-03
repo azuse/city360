92b5316ba0c448e75ec795e03d96e4740f296607
azuse <misakaxindex@gmail.com>
2018-05-03 19:50:41 +0800

    detailwindow

--- showall/showall.js
+++ showall/showall.js
@@ -4,7 +4,7 @@ function newcard(name,author,street,tel,budget,demand,time,address,lng,lat){
     var col = document.createElement("div");
     col.className = "col s12 m6";
     var card = document.createElement("div");
-    card.className = "card blue-grey darken-1"
+    card.className = "card blue-grey darken-1 hoverable"
     var container = document.createElement("div");
     // container.className = "container";
     var row = document.createElement("div");
@@ -14,40 +14,46 @@ function newcard(name,author,street,tel,budget,demand,time,address,lng,lat){
     namediv.className = "col s12 m12";
     var nameh5 = document.createElement("h5");
     nameh5.innerHTML = '<i class="material-icons prefix">title</i> ' + name;
+    nameh5.className = "truncate";
     namediv.appendChild(nameh5);
 
     var authordiv = document.createElement("div");
     authordiv.className = "col s12 m6";
     var authorh5 = document.createElement("p");
     authorh5.innerHTML = '<i class="material-icons prefix">account_circle</i> ' + author;
+    authorh5.className = "truncate";
     authordiv.appendChild(authorh5);
 
     var teldiv = document.createElement("div");
     teldiv.className = "col s12 m6";
     var telp = document.createElement("p");
     telp.innerHTML = '<i class="material-icons prefix">phone</i> ' + tel;
+    telp.className = "truncate";
     teldiv.appendChild(telp);
 
     var budgetdiv = document.createElement("div");
     budgetdiv.classList = "col s12 m6";
     var budgetp = document.createElement("p");
     budgetp.innerHTML = '<i class="material-icons prefix">monetization_on</i> ' + budget;
+    budgetp.className = "truncate";
     budgetdiv.appendChild(budgetp);
 
     var streetdiv = document.createElement("div");
     streetdiv.className = "col s12 m6";
     var streetp = document.createElement("p");
     streetp.innerHTML = '<i class="material-icons prefix">domain</i> ' + street;
+    streetp.className = "truncate";
     streetdiv.appendChild(streetp);
 
     var demanddiv = document.createElement("div");
     demanddiv.className = "col s12 m6";
     var demandp = document.createElement("p");
     demandp.innerHTML = '<i class="material-icons prefix">star</i> ' +  demand;
+    demandp.className = "truncate";
     demanddiv.appendChild(demandp);
 
     var addressdiv = document.createElement("div");
-    addressdiv.className = "col s12 m6";
+    addressdiv.className = "col s12 m6 truncate";
     addressdiv.innerHTML = '<p><i class="material-icons prefix">place</i> ' + address + "</p>";
 
     row.appendChild(namediv);
@@ -63,7 +69,18 @@ function newcard(name,author,street,tel,budget,demand,time,address,lng,lat){
     var cardaction = document.createElement("div");
     cardaction.className = "card-action";
     var a = document.createElement('a');
-    a.href = "#detail";
+    // a.href = "#detail";
+    a.href = 'javascript:void(0)';
+    a.onclick = function (){
+        $("#detail_name")[0].innerHTML = '<i class="material-icons prefix margin14">title</i>'+name;
+        $("#detail_author")[0].innerHTML = '<i class="material-icons prefix margin14">account_circle</i>'+author;
+        $("#detail_street")[0].innerHTML = '<i class="material-icons prefix margin14">domain</i>'+street;
+        $("#detail_tel")[0].innerHTML = '<i class="material-icons prefix margin14">phone</i>'+tel;
+        $("#detail_budget")[0].innerHTML = '<i class="material-icons prefix margin14">monetization_on</i>'+budget;
+        $("#detail_demand")[0].innerHTML = '<i class="material-icons prefix margin14">star</i>'+demand;
+        $("#detail_address")[0].innerHTML = '<i class="material-icons prefix margin14">place</i>'+address;
+        $("#detail_window").modal('open');
+    };
     a.innerHTML = "详情"
     cardaction.appendChild(a);
 
