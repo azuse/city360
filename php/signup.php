d872c6a563752a5c99da4cf6b415e9918a0bdec9
azuse <misakaxindex@gmail.com>
2018-05-21 22:00:01 +0800

    php分文件

--- php/signup.php
+++ php/signup.php
@@ -1,27 +1,5 @@
 <?php
-    class DB{
-        public $conn;
-        function __construct()
-        { //connect
-            $dbhost = "localhost";
-            $account = "root";
-            $password = "misakaxindex";
-            $dbname = "city360";
-            
-            $this->conn = mysql_connect($dbhost,$account,$password);        
-            if (!$this->conn) {
-                // die('Connect Error (' . mysql_connect_errno() . ') '. mysql_connect_error());
-            }else{
-                //echo 'Connect success... ' . mysqli_get_host_info($this->conn) . "\n";
-            }
-        
-        }
-        function __destruct()
-        {   //disconnect
-            //mysqli_colse($this->conn);//无此函数
-        }
-
-    }
+    require_once("city360.php");
     
     function signup($item){
         
