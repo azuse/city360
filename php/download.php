72320be61e15d40a05d0a439836510e069124abd
azuse <misakaxindex@gmail.com>
2018-06-02 15:50:18 +0800

    debug

--- php/download.php
+++ php/download.php
@@ -81,7 +81,7 @@ function loadData(){
             foreach ($files as $value) {
                 $files_str = $files_str.'"'.$value.'",';
             }
-            $temp["files"] = $files_str."]";
+            $temp["files"] = $files
         }
         //放入二维数组dataBuf中
         $dataBuf[$i++] = $temp;
