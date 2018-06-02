aa21b541df57ef3f0672ad64528ae2a494579c89
azuse <misakaxindex@gmail.com>
2018-06-02 19:06:41 +0800

    debug

--- php/downloadDesign.php
+++ php/downloadDesign.php
@@ -22,6 +22,27 @@
         }
     }
 
+    //获取文件列表
+    function getFile($dir) {
+        $fileArray[]=NULL;
+        if (false != ($handle = opendir ( $dir ))) {
+            $i=0;
+            while ( ($file = readdir($handle)) !== false ) {
+                //去掉"“.”、“..”以及带“.xxx”后缀的文件
+                if ($file != "." && $file != "..") {
+                    $fileArray[$i]=$file;
+                    if($i==100){
+                        break;
+                    }
+                    $i++;
+                }
+            }
+            //关闭句柄
+            closedir ( $handle );
+        }
+        return $fileArray;
+    }
+
     function loadData($designproject){
         $db = new DB();
         
