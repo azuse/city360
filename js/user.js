50b706a2124cf1cd166b78545753f01247378e5f
azuse <misakaxindex@gmail.com>
2018-07-13 01:52:55 +0800

    update

--- js/user.js
+++ js/user.js
@@ -1,4 +1,6 @@
 //用户操作
+var userSigninURL = "php/usersignin.php";
+
 function newuser(){
     var username = $("username").value;
     var mailaddr = $("mailaddr").value;
@@ -12,5 +14,29 @@ function newuser(){
         return;
     }
 
+    $.ajax({
+        url:userSigninURL,
+        type: "post",
+        dataType:"text",
+        async: false,
+        data: {
+            "nickname":username,
+            "password":password,
+            "email":mailaddr,
+            ""
+        },
+        success: function(result){
+            alert("上传成功");
+            $("#designername")[0].value = "";
+            $("#designertel")[0].value = "";
+            
+
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown){
+            alert(XMLHttpRequest.status);
+            alert(XMLHttpRequest.readyState);
+            alert(textStatus);
+        }
+    });
     
 }
