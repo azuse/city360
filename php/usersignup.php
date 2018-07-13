8d2857959c073ed08afa5f6d3de01aad1f997388
azuse <misakaxindex@gmail.com>
2018-07-13 23:53:50 +0800

    debug

--- php/usersignup.php
+++ php/usersignup.php
@@ -1,6 +1,7 @@
 <?php
     header('Content-type: text/html;charset=UTF-8');
-
+    ini_set('display_errors',1);
+    error_reporting(E_ALL);
 
     class Item{
         public $nickname,$password,$email,$job,$jobdetail,$tel,$birth,$avatar;
