21bfb043c8e6c8c24edf206af1cd92d19fc42a9d
azuse <misakaxindex@gmail.com>
2018-08-13 14:53:56 +0800

    debug

--- php/adddesign.php
+++ php/adddesign.php
@@ -27,10 +27,14 @@ class DB{
 
 $time = date("Y-m-d H:s:i");
     if($_SERVER['REQUEST_METHOD']=='POST'){
+        $file = $_FILES['file'];//得到传输的数据
+        //得到文件名称
+        $name = $file['name'];
+
         $db = new DB();
     	$sqlselect = "USE city360";
         $result = mysql_query($sqlselect,$db->conn);
-        $sql = "INSERT INTO `designdatabase`(`name`,`shape`,`size`,`type`,`designage`,`outdoor`,`district`,`region`,`function`,`detail`,`img`) VALUES ('".$_POST['name']."','".$_POST['shape']."','".$_POST['size']."','".$_POST['type']."','".$_POST['designage']."','".$_POST['outdoor']."','".$_POST['district']."','".$_POST['region']."','".$_POST['function']."','".$_POST['detail']."','".$_FILES["file"]["name"]."');";
+        $sql = "INSERT INTO `designdatabase`(`name`,`shape`,`size`,`type`,`designage`,`outdoor`,`district`,`region`,`function`,`detail`,`img`) VALUES ('".$_POST['name']."','".$_POST['shape']."','".$_POST['size']."','".$_POST['type']."','".$_POST['designage']."','".$_POST['outdoor']."','".$_POST['district']."','".$_POST['region']."','".$_POST['function']."','".$_POST['detail']."','".$name."');";
         move_uploaded_file($_FILES["file"]["tmp_name"],"designdataimg/" . $_FILES["file"]["name"]);
 
     };
