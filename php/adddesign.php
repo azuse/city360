ecfaa5a97250e2bb9df6c8fc62b0e8fa6fac2a05
azuse <misakaxindex@gmail.com>
2018-08-13 14:48:08 +0800

    debug

--- php/adddesign.php
+++ php/adddesign.php
@@ -31,7 +31,7 @@ $time = date("Y-m-d H:s:i");
     	$sqlselect = "USE city360";
         $result = mysql_query($sqlselect,$db->conn);
         $sql = "INSERT INTO `designdatabase`(`name`,`shape`,`size`,`type`,`designage`,`outdoor`,`district`,`region`,`function`,`detail`,`img`) VALUES ('".$_POST['name']."','".$_POST['shape']."','".$_POST['size']."','".$_POST['type']."','".$_POST['designage']."','".$_POST['outdoor']."','".$_POST['district']."','".$_POST['region']."','".$_POST['function']."','".$_POST['detail']."','".$_FILES["file"]["name"]."');";
-
+        move_uploaded_file($_FILES["file"]["tmp_name"],"designdataimg/" . $_FILES["file"]["name"]);
 
     };
 ?>
