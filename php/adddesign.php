c2d539de61cd89e4d9a3ec872e0d1b610c7de26c
azuse <misakaxindex@gmail.com>
2018-08-13 16:54:24 +0800

    debug

--- php/adddesign.php
+++ php/adddesign.php
@@ -34,6 +34,9 @@ $time = date("Y-m-d H:s:i");
         $db = new DB();
     	$sqlselect = "USE city360";
         $result = mysql_query($sqlselect,$db->conn);
+        $sqlsetutf8 = "set names 'utf8'";
+        $result = mysql_query($sqlsetutf8,$db->conn);
+
         $sql = "INSERT INTO `designdatabase`(`name`,`shape`,`size`,`type`,`designage`,`outdoor`,`district`,`region`,`function`,`detail`,`img`) VALUES ('".$_POST['name']."','".$_POST['shape']."','".$_POST['size']."','".$_POST['type']."','".$_POST['designage']."','".$_POST['outdoor']."','".$_POST['district']."','".$_POST['region']."','".$_POST['function']."','".$_POST['detail']."','".$name."');";
         echo $sql;
         $result = mysql_query($sql,$db->conn);    
